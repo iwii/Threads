1c546d7a926931cacdf49a3501c51cf1f2298958
IMATUJEW <iwona.matujewicz-nizialek@capgemini.com>
2019-02-21 11:15:26 +0100

    remove class implements

--- src/main/java/com/example/threads/controller/PingPongThread.java
+++ src/main/java/com/example/threads/controller/PingPongThread.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//public class PingPongThread implements Runnable {
 public class PingPongThread extends Thread{
 
 
